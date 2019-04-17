e9f145841ceb633382f324d82d8a301e8bfc92ae
Yoshihiro Yamada <yoshi@tectech.jp>
2019-04-17 18:37:29 +0900

    ディレクトリ削除関数rmdir追加

--- src/Util.php
+++ src/Util.php
@@ -1,9 +1,8 @@
 <?php
-
 class Util{
 	//===========================================
-	//SQL文文字列用
-	//第1引数にシングルコーテーションをつけてリターン
+	// SQL文文字列用
+	// 第1引数にシングルコーテーションをつけてリターン
 	//===========================================
 	public static function SqlStr($str, $nullStr=null){
 		if(isset($str) && is_string($str)){
@@ -23,8 +22,8 @@ class Util{
 	}
 	
 	//===========================================
-	//SQL文LIKE文字列用
-	//第1引数
+	// SQL文LIKE文字列用
+	// 第1引数
 	//===========================================
 	public static function SqlLike($str){
 		if(isset($str) && is_string($str)){
@@ -39,7 +38,7 @@ class Util{
 	}
 	
 	//===========================================
-	//SQL文数値用
+	// SQL文数値用
 	//===========================================
 	public static function SqlNum($num, $nullNum=null){
 		if(isset($num) && is_numeric($num)){
@@ -61,12 +60,28 @@ class Util{
 	}
 	
 	//==================================================
-	//全角スペースと半角スペースのtrim
+	// 全角スペースと半角スペースのtrim
 	//==================================================
 	public static function getTrim($str){
 		if(isset($str) && is_string($str)){
 			$str = trim($str);
 			return preg_replace('/^[ 　]*(.*?)[ 　]*$/u', '$1', $str);
+		}
+	}
+
+	
+	//==================================================
+	// ディレクトリを削除
+	//==================================================
+	public static function rmdir($dir){
+		if (is_dir($dir) and !is_link($dir)) {
+			$paths = array();
+			while ($glob = glob($dir)) {
+				$paths = array_merge($glob, $paths);
+				$dir .= '/*';
+			}
+			array_map('unlink', array_filter($paths, 'is_file'));
+			array_map('rmdir',  array_filter($paths, 'is_dir'));
 		}
 	}
 	
